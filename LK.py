0852fbecdc513c2dc2247c6a87da1b09f15544d9
Farklin <67648571+Farklin@users.noreply.github.com>
2020-12-03 17:52:41 +0300

    Add files via upload

--- LK.py
+++ LK.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import time 
 import openpyxl
 import csv
+from poisk_elementov import Page 
 
 
 class Key: 
@@ -35,23 +36,38 @@ class Landing_Page:
             if int(key.position) > 10 or int(key.position) == 0: 
                 print(key.position, key.phraze)
 
+    def meta(self): 
+        if self.url != '': 
+            page = Page(self.url)
+            page.get_meta()
+            page.get_h1()
+
+            self.title = page.title
+            self.description = page.description
+            self.keywords = page.keywords
+
+            self.h1 = page.h1
+        else: 
+            self.title = 'Старницы не сущесвтует '
+            self.description = 'Старницы не сущесвтует'
+            self.keywords = 'Старницы не сущесвтует'
+            self.h1 = 'Старницы не сущесвтует'
 
 class LK_Command: 
 
     # логин и пароль, авторизация 
     def __init__(self, login, password): 
         pass
-        # self.login = login
-        # self.password = password
+        self.login = login
+        self.password = password
     
-        # session = requests.Session()
-        # url = 'http://an.rem-mach.ru/login.php'
-        # dann = dict(user_login = self.login, user_password = self.password)
-        # session.get(url)
-        # session.post(url, dann)
+        session = requests.Session()
+        url = 'http://an.rem-mach.ru/login.php'
+        dann = dict(user_login = self.login, user_password = self.password)
+        session.get(url)
+        session.post(url, dann)
 
-        # self.session = session
-
+        self.session = session
 
     def set_site_url(self):
         con = self.session.get("http://an.rem-mach.ru/edit_site_tab_url.php?site_id=" + self.number)
@@ -82,6 +98,7 @@ class LK_Command:
         self.regions_tr = regions 
 
         for region in regions: 
+            self.region = region
             con = self.session.get("http://an.rem-mach.ru/keys_list.php?download_list=1&site_id="+self.number+"&site_stat_region="+str(region))
             f=open(r''+ self.site_url + str(region) + '.csv',"wb")
             f.write(con.content)
@@ -90,7 +107,7 @@ class LK_Command:
     def analization_TR(self):
         
         compilation = [] 
-        with open('waycon.ru615' +'.csv', newline='') as File:  
+        with open(self.site_url+str(self.region) +'.csv', newline='') as File:  
             reader = csv.reader(File)
             for val, row in enumerate(reader):
                 if val != 0: 
@@ -113,8 +130,7 @@ class LK_Command:
     def get_praze_down(self):
         for lp in self.landing_pages:
             lp.down_position()
-           
-
+        
     def group_phraze_in_url(self):
         mas_pages = []
         self.landing_pages = [] 
@@ -131,8 +147,43 @@ class LK_Command:
                         keys.add(key)
                 lp = Landing_Page(url, keys)
                 self.landing_pages.append(lp)
+    def meta(self): 
+        for lp in self.landing_pages: 
+            lp.meta() 
+            try: 
+                print(lp.url)
+                print(lp.title) 
+                print(lp.description) 
+                print(lp.keywords)
+            except: 
+                pass
+
+    def reported_struct(self, name_file):     
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+        for val, lp in enumerate(self.landing_pages): 
+           
+            try: 
+                cell = sheet.cell(row=val, column=len(lp.url.split('/'))-2)
+                cell.hyperlink = lp.url 
+                cell.value = lp.h1 
+            except: 
+                pass 
+        wb.save(name_file + ".xlsx")
+
+    def excel(self, mas, name_file): 
+        wb = openpyxl.Workbook()
+        sheet = wb.active
+
+        for row, product in enumerate(mas): 
+            for col, key in enumerate(product.keys()): 
+                cell = sheet.cell(row=row+1, column=col+1) 
+                cell.value = product[key]
+        wb.save(name_file + ".xlsx")
+
 
 user = LK_Command('Jilcov', 'Jilcov435333')
-#user.downland_TR('5202')
+user.downland_TR('4028')
 user.analization_TR()   
-user.get_praze_down()
+user.meta()
+user.reported_struct('struct_studiomix') 
